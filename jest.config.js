d66003dd6d0dbf8def794fb55796097b52ee99c3
Haixing Hu <starfish.hu@gmail.com>
2023-12-18 16:45:17 +0800

    feat: check in initial codes

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (c) 2022 - 2023.
+//    Haixing Hu, Qubit Co. Ltd.
+//
+//    All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+module.exports = {
+  verbose: true,
+  testMatch: ['**/test/**/*.test.js'],
+  testEnvironment: 'jest-environment-jsdom-global',
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons'],
+  },
+  moduleFileExtensions: ['js'],
+  moduleDirectories: ['node_modules'],
+  transform: {
+    '\\.js$': 'babel-jest',
+    // '^.+\\.vue$': '@vue/vue3-jest',
+  },
+  setupFilesAfterEnv: [
+    'jest-extended/all',
+  ],
+  collectCoverage: true,
+  coverageDirectory: './coverage',
+  collectCoverageFrom: ['**/src/**/*.js'],
+};
