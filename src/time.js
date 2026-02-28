ae21997903304462f47cd740295491df01d4935b
Haixing Hu <starfish.hu@gmail.com>
2026-02-28 11:01:47 +0800

    refactor: 优化 formatTime 的参数处理与边界检查

--- src/time.js
+++ src/time.js
@@ -22,12 +22,11 @@ import toTimezone from './to-timezone';
  *     input is `null` or `undefined` or empty string, this function returns `null`.
  * @author Haixing Hu
  */
-function formatTime(dateTime, tz = undefined) {
-  if (dateTime) {
-    return toTimezone(dateTime, tz).format('HH:mm:ss');
-  } else {
+function formatTime(dateTime, tz) {
+  if (!dateTime) {
     return null;
   }
+  return toTimezone(dateTime, tz).format('HH:mm:ss');
 }
 
 export default formatTime;
