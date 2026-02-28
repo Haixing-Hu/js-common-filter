9967cbe687ed149269c1eb003b30e08b1a0e157a
Haixing Hu <starfish.hu@gmail.com>
2026-02-28 10:12:14 +0800

    refactor: 优化 arrayToString 函数的实现，提高代码可读性和性能

--- src/array-to-string.js
+++ src/array-to-string.js
@@ -15,7 +15,7 @@ const DEFAULT_SEPARATOR = ' ';
 /**
  * Converts the specified array to a string with the specified separator.
  *
- * @param {array} array
+ * @param {Array} array
  *     The array to be converted.
  * @param {string} separator
  *     The separator, if not specified, the default separator will be used.
@@ -24,18 +24,13 @@ const DEFAULT_SEPARATOR = ' ';
  * @author Haixing Hu
  */
 function arrayToString(array, separator) {
-  if (array === undefined || array === null || array.length === 0) {
+  if (!array || array.length === 0) {
     return '';
   }
-  if (!separator) {
-    separator = DEFAULT_SEPARATOR;
-  }
-  let result = '';
-  for (let i = 0; i < array.length; ++i) {
-    if (i > 0) {
-      result += separator;
-    }
-    result += String(array[i]);
+  const sep = (separator ?? '') === '' ? DEFAULT_SEPARATOR : separator;
+  let result = String(array[0]);
+  for (let i = 1; i < array.length; ++i) {
+    result += sep + String(array[i]);
   }
   return result;
 }
