b8a8c74098934893a78f88f3d159c7c08fa0bbd1
Haixing Hu <starfish.hu@gmail.com>
2026-02-28 10:55:32 +0800

    refactor: 优化 dateToEndTimestamp 中的边界检查和默认参数

--- src/date-to-end-timestamp.js
+++ src/date-to-end-timestamp.js
@@ -26,8 +26,8 @@ import { DATE_REGEXP } from './impl/regexp';
  *     local timezone; or an empty string if the input is `null` or `undefined` or
  *     empty string.
  */
-function dateToEndTimestamp(date, tz = undefined) {
-  if (date === null || date === undefined || date === '') {
+function dateToEndTimestamp(date, tz) {
+  if (date == null || date === '') {
     return '';
   }
   if (!DATE_REGEXP.test(date)) {
